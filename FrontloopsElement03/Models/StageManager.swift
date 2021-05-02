72c9c6e7dd1337cda2bed5234fea4c8775b8c0ba
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-05-02 10:40:27 +0900

    Really done

--- FrontloopsElement03/Models/StageManager.swift
+++ FrontloopsElement03/Models/StageManager.swift
@@ -17,23 +17,23 @@ struct StageManager {
     var currentStage: Int?
     var delegate: StageManagerDelegate?
     mutating func setStage(for stageNumber: Int) {
-        switch stageNumber {
-        case 0:
-            delegate?.removeSecondSubmitButton(self)
-        case 1:
-            delegate?.removeSecondSubmitButton(self)
-            delegate?.updateSecondSubmitButton(self, with: "Back")
-        case 2:
-            delegate?.removeSecondSubmitButton(self)
-            delegate?.updateSecondSubmitButton(self, with: "No, go back")
-        default:
-            print("Stage number out of range")
-        }
         if stageNumber > 2 {
             print("Stage number out of range")
             return
         }
         if abs((self.currentStage ?? 0) - stageNumber) <= 1 {
+            switch stageNumber {
+            case 0:
+                delegate?.removeSecondSubmitButton(self)
+            case 1:
+                delegate?.removeSecondSubmitButton(self)
+                delegate?.updateSecondSubmitButton(self, with: "Back")
+            case 2:
+                delegate?.removeSecondSubmitButton(self)
+                delegate?.updateSecondSubmitButton(self, with: "No, go back")
+            default:
+                print("Stage number out of range")
+            }
             self.currentStage = stageNumber
             self.delegate?.updateUI(self, for: self.currentStage!)
         }
